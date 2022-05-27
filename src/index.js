d9eb7e75538a4717374e0d51c31db2173eb9ee86
Kevin Peng <pengkevi@amazon.com>
2022-05-27 16:27:43 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import AWSSfnGraph from '@tshepomgaga/aws-sfn-graph';
+import '@tshepomgaga/aws-sfn-graph/index.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const sampleData = {
+  Comment:
+    'A Hello World example of the Amazon States Language using Pass states',
+  StartAt: 'Hello',
+  States: {
+    Hello: {
+      Type: 'Pass',
+      Result: 'Hello',
+      Next: 'World'
+    },
+    World: {
+      Type: 'Pass',
+      Result: 'World',
+      End: true
+    }
+  }
+}
+
+const App = () => {
+  const [aslData, setAslData] = useState(sampleData)
+  function handleChange(e) {
+	const jsonText = e.target.value
+	try {
+		const object = JSON.parse(jsonText)
+		if (object != null) {
+			setAslData(object)
+		} else {
+			setAslData(sampleData)
+		}
+	} catch(error) {
+		setAslData(sampleData)
+	}
+  }
+  return (
+	<>
+      <input 
+      type="text"
+      name="stateMachineJson"
+      onChange={ handleChange }  />
+      <AWSSfnGraph
+        data={aslData}
+        width={800}
+        height={800}
+        onError={console.log}
+      />
+	</>
+  )
+}
+
+export default App
+
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
